1bd9e7c8528fd47b74a0079e4f6577b2241f6dde
Lisa Umhey <lisa.umhey@kazoohr.com>
2021-02-20 20:17:59 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { ThemeProvider } from "@material-ui/styles";
+import {
+  Box,
+  Typography,
+  Container,
+  Button,
+  createMuiTheme,
+  TextField,
+} from "@material-ui/core";
+
+import "./App.css";
+
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontFamily: "Merriweather",
+    },
+    button: {
+      fontFamily: "Inter",
+    },
+  },
+});
 
 function App() {
+  const [value, setValue] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [helperText, setHelperText] = useState("");
+
+  function isValidUSZip(zipCode) {
+    return /^\d{5}(-\d{4})?$/.test(zipCode);
+  }
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    setShowError(false);
+    setHelperText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          m={3}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant="h2">WTF is the weather like now?</Typography>
+          <Box m={1}></Box>
+          <TextField
+            error={showError}
+            id="outlined-error-helper-text"
+            label="Zip code"
+            value={value}
+            helperText={helperText}
+            onChange={handleChange}
+            variant="outlined"
+          />
+          <Box m={1}></Box>
+          <Button
+            variant="contained"
+            disabled={!value}
+            onClick={() => {
+              if (!isValidUSZip(value)) {
+                setShowError(true);
+                setHelperText("Please enter a valid US zip code.");
+              }
+            }}
+          >
+            Let's see...
+          </Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
